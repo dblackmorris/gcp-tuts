7324d22dda095f6bb1194f457a0b408094feeb4e
Ritik Dubey <ritik.dy@gmail.com>
2019-08-28 16:18:02 +0530

    Update gcp_commands.sh

---
+++ gcp_commands.sh
@@ -0,0 +1,6 @@
+#GSutil commands
+gsutil ls #list bucket
+gsutil mb -l us-east1 gs://gcp-ls-d4nt3 #where mb = make bucket, -l = location,gs://gcp-ls-d4nt3 = url for new bucket
+gsutil cp README-cloudshell.txt gs://gcp-ls-d4nt3/ #where cp=copy 
+gsutil ls gs://gcp-ls-d4nt3/ # where ls is list and used here for list all object inside the bucket
+gsutil cp README-cloudshell.txt gs://gcp-ls-d4nt3/test/ # this command will be used for copy file inside test directory if present or create dir named test and then copy file inside that
