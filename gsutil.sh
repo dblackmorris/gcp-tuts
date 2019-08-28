cae5f8caffd0b80f6658c9dfd78df625aa8d2723
Ritik Dubey <ritik.dy@gmail.com>
2019-08-28 17:08:53 +0530

    Add versioning related commands on gsutil

--- gsutil.sh
+++ gsutil.sh
@@ -6,3 +6,6 @@ gsutil ls gs://gcp-ls-d4nt3/ # where ls is list and used here for list all objec
 gsutil cp README-cloudshell.txt gs://gcp-ls-d4nt3/test/ # this command will be used for copy file inside test directory if present or create dir named test and then copy file inside that
 gsutil label get gs://gcp-ls-d4nt3/ # for getting tag configuration of that bucket
 gsutil label set bucketlabels.json gs://gcp-ls-d4nt3/ # for setting tag on that bucket where bucketlabel.json is file with all tagging information
+gsutil label ch -l "extralabel:extravalue" gs://gcp-ls-d4nt3/ # for adding extra tag value where ch = change, -l for label
+gsutil versioning get gs://gcp-ls-d4nt3/ # command for get versioning status
+gsutil versioning set on gs://gcp-ls-d4nt3/ # command for set versioning on/off
