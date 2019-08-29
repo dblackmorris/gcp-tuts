6c2657d6176eb7791c6f50f98b47c0d806d2fc55
Ritik Dubey <ritik.dy@gmail.com>
2019-08-29 23:57:29 +0530

    add file

---
+++ compute_instances.sh
@@ -0,0 +1,7 @@
+gcloud config get-value project # for getting the name of selected project
+gcloud compute instances list # For first time you need to enable the compute API.
+gcloud services list --enabled # For all enabled api services
+gcloud services list --available # For all available API
+gcloud compute instances create myvm # For creating new compute instance named myvm
+gcloud compute instances delete myvm # For deleting compute instance named myvm
+
